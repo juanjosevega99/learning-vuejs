dfbb0471f596a29519825ae19b510d4c4803c7da
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-24 14:47:47 -0500

    [ADD] list rendering

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ new Vue({
     data () {
         return {
             name: 'Bitcoin',
-            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: 10
+            // img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+            changePercent: 10,
+            prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+
+            pricesWithDays: [
+                { day: 'Lunes', value: 8400 },
+                { day: 'Martes', value: 7900 },
+                { day: 'Miercoles', value: 8200 },
+                { day: 'Jueves', value: 9000 },
+                { day: 'Viernes', value: 9400 },
+                { day: 'Sabado', value: 10000 },
+                { day: 'Domingo', value: 10200 },
+            ]
         }
     }
 })
